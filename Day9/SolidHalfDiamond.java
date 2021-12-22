5f4565e797ff16d2d01922ab8d417961cf01e10c
unknown <chinthajahnavi240100@gmail.com>
2021-12-22 22:39:29 +0530

    javaprogram

---
+++ Day9/SolidHalfDiamond.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class SolidHalfDiamond {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+        for(int i=1;i<n;i++){
+            for(int j=n-1;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+
+        sc.close();
+}
+}
+
+OUTPUT-
+5
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
---
+++ Day9/SolidRectangle1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class SolidRectangle1
+{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+     {
+      for(int j=1;j<=5;j++)
+      {
+       System.out.print("* ");
+      }
+      System.out.println( );
+     }
+     sc.close();
+  }
+}
+
+OUTPUT-
+3
+* * * * * 
+* * * * *
+* * * * *
